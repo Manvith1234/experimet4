d6e505bcf1c67e6f3c923c9fdc70c736e1d61da9
Manvith <mjraip06@gmail.com>
2026-02-20 09:57:28 +0530

    Update main.py to call add, sub, and mul functions

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+from add import add
+from sub import sub
+from mul import mul
+
 def main():
-    # call add()
-    # call sub()
-    # call mul()
-    pass
+    print("Addition (5, 3):", add(5, 3))
+    print("Subtraction (5, 3):", sub(5, 3))
+    print("Multiplication (5, 3):", mul(5, 3))
 
 if __name__ == "__main__":
     main()
